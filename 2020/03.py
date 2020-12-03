84e1ec55b8bf9c3f25c074226d1d55471618a6c7
Clayton Smith <argilo@gmail.com>
2020-12-03 00:09:40 -0500

    Day 3 in Python

---
+++ 2020/03.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Copyright 2020 Clayton Smith
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+l = []
+
+for line in open("03-input.txt"):
+    l.append(line.rstrip())
+
+def slope(right, down):
+    count = 0
+    for i in range(len(l) // down):
+        row = l[i * down]
+        if row[(i * right) % len(row)] == "#":
+            count += 1
+    return count
+
+print(slope(3, 1))
+print(slope(1, 1) * slope(3, 1) * slope(5, 1) * slope(7, 1) * slope(1, 2))
