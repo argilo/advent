d0c747b6191d39e45f2151f4035f4348f3e25b86
Clayton Smith <argilo@gmail.com>
2021-12-08 00:23:33 -0500

    Day 8 in Python

---
+++ 2021/08.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Clayton Smith
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import aocd
+import itertools
+
+data = aocd.get_data(day=8, year=2021)
+# data = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+# edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+# fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+# fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+# aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+# fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+# dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+# bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+# egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+# gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
+
+
+digits = {
+    0: "abcefg",
+    1: "cf",
+    2: "acdeg",
+    3: "acdfg",
+    4: "bcdf",
+    5: "abdfg",
+    6: "abdefg",
+    7: "acf",
+    8: "abcdefg",
+    9: "abcdfg",
+}
+
+digits = {
+    "abcefg": 0,
+    "cf": 1,
+    "acdeg": 2,
+    "acdfg": 3,
+    "bcdf": 4,
+    "abdfg": 5,
+    "abdefg": 6,
+    "acf": 7,
+    "abcdefg": 8,
+    "abcdfg": 9,
+}
+
+tot = 0
+tot2 = 0
+for line in data.splitlines():
+    parts = line.split(" | ")
+    calib = ["".join(sorted(s)) for s in parts[0].split()]
+    target = ["".join(sorted(s)) for s in parts[1].split()]
+    print(calib, target)
+    for p in itertools.permutations("ABCDEFG"):
+        tr = str.maketrans("abcdefg", "".join(p))
+        translated = ["".join(sorted(c.translate(tr).lower())) for c in calib]
+        if all(t in digits for t in translated):
+            translated2 = [digits["".join(sorted(c.translate(tr).lower()))] for c in target]
+            print(translated2)
+            tot += translated2.count(1) + translated2.count(4) + translated2.count(7) + translated2.count(8)
+            tot2 += int("".join(str(n) for n in translated2))
+print(tot)
+aocd.submit(tot, part="a", day=8, year=2021)
+
+print(tot2)
+aocd.submit(tot2, part="b", day=8, year=2021)
