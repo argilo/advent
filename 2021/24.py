109930b2ae4045c4061b89f06adece490ab11f49
Clayton Smith <argilo@gmail.com>
2021-12-24 22:51:46 -0500

    General solution for Day 24

--- 2021/24.py
+++ 2021/24.py
@@ -20,74 +20,101 @@ import aocd
 data = aocd.get_data(day=24, year=2021)
 lines = data.splitlines()
 
+stack = []
+ans1 = [None]*14
+ans2 = [None]*14
+for index, group in enumerate(range(0, len(lines), 18)):
+    push = (lines[group+4]) == "div z 1"
+    add_x = int(lines[group+5].split()[2])
+    add_y = int(lines[group+15].split()[2])
 
-def calc(input):
-    reg = [0, 0, 0, 0]
-    reg_name = ["w", "x", "y", "z"]
-
-    for line in lines:
-        parts = line.split()
-        ins = parts[0]
-        reg_index = reg_name.index(parts[1])
-
-        if ins == "inp":
-            reg[reg_index] = input.pop(0)
+    if push:
+        stack.append((index, add_y))
+    else:
+        other_index, add_y = stack.pop()
+        diff = add_x + add_y
+        if diff >= 0:
+            ans1[other_index] = 9 - diff
+            ans1[index] = 9
+            ans2[other_index] = 1
+            ans2[index] = 1 + diff
         else:
-            arg = parts[2]
-            if arg in reg_name:
-                arg = reg[reg_name.index(arg)]
-            else:
-                arg = int(arg)
+            ans1[other_index] = 9
+            ans1[index] = 9 + diff
+            ans2[other_index] = 1 - diff
+            ans2[index] = 1
 
-            if ins == "add":
-                reg[reg_index] += arg
-            elif ins == "mul":
-                reg[reg_index] *= arg
-            elif ins == "div":
-                reg[reg_index] //= arg
-            elif ins == "mod":
-                reg[reg_index] %= arg
-            elif ins == "eql":
-                reg[reg_index] = (1 if arg == reg[reg_index] else 0)
-    return reg[3]
+print("".join(str(n) for n in ans1))
+print("".join(str(n) for n in ans2))
 
 
-for d0 in range(1, 6):
-    d13 = d0 + 4
-
-    for d1 in (1,):
-        d12 = d1 + 8
-
-        for d2 in (7, 8, 9):
-            d5 = d2 - 6
-
-            for d3 in (1, 2, 3):
-                d4 = d3 + 6
-
-                for d6 in range(3, 10):
-                    d7 = d6 - 2
-
-                    for d8 in range(2, 10):
-                        d9 = d8-1
-
-                        for d10 in range(1, 10):
-                            d11 = d10
-
-                            input = [None]*14
-                            input[0] = d0
-                            input[1] = d1
-                            input[2] = d2
-                            input[3] = d3
-                            input[4] = d4
-                            input[5] = d5
-                            input[6] = d6
-                            input[7] = d7
-                            input[8] = d8
-                            input[9] = d9
-                            input[10] = d10
-                            input[11] = d11
-                            input[12] = d12
-                            input[13] = d13
-                            input_str = "".join(str(n) for n in input)
-                            result = calc(input)
-                            print(input_str, result)
+# def calc(input):
+#     reg = [0, 0, 0, 0]
+#     reg_name = ["w", "x", "y", "z"]
+#
+#     for line in lines:
+#         parts = line.split()
+#         ins = parts[0]
+#         reg_index = reg_name.index(parts[1])
+#
+#         if ins == "inp":
+#             reg[reg_index] = input.pop(0)
+#         else:
+#             arg = parts[2]
+#             if arg in reg_name:
+#                 arg = reg[reg_name.index(arg)]
+#             else:
+#                 arg = int(arg)
+#
+#             if ins == "add":
+#                 reg[reg_index] += arg
+#             elif ins == "mul":
+#                 reg[reg_index] *= arg
+#             elif ins == "div":
+#                 reg[reg_index] //= arg
+#             elif ins == "mod":
+#                 reg[reg_index] %= arg
+#             elif ins == "eql":
+#                 reg[reg_index] = (1 if arg == reg[reg_index] else 0)
+#     return reg[3]
+#
+#
+# for d0 in range(1, 6):
+#     d13 = d0 + 4
+#
+#     for d1 in (1,):
+#         d12 = d1 + 8
+#
+#         for d2 in (7, 8, 9):
+#             d5 = d2 - 6
+#
+#             for d3 in (1, 2, 3):
+#                 d4 = d3 + 6
+#
+#                 for d6 in range(3, 10):
+#                     d7 = d6 - 2
+#
+#                     for d8 in range(2, 10):
+#                         d9 = d8-1
+#
+#                         for d10 in range(1, 10):
+#                             d11 = d10
+#
+#                             input = [None]*14
+#                             input[0] = d0
+#                             input[1] = d1
+#                             input[2] = d2
+#                             input[3] = d3
+#                             input[4] = d4
+#                             input[5] = d5
+#                             input[6] = d6
+#                             input[7] = d7
+#                             input[8] = d8
+#                             input[9] = d9
+#                             input[10] = d10
+#                             input[11] = d11
+#                             input[12] = d12
+#                             input[13] = d13
+#                             input_str = "".join(str(n) for n in input)
+#                             result = calc(input)
+#                             print(input_str, result)
