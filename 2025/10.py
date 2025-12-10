69677cc8bb9e0cf46076a6ac355f0ce84971aea1
Clayton Smith <argilo@gmail.com>
2025-12-10 00:46:18 -0500

    Day 10 in Python and Sage

---
+++ 2025/10.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# Copyright 2025 Clayton Smith
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import aocd
+import itertools
+
+YEAR = 2025
+DAY = 10
+
+data = aocd.get_data(day=DAY, year=YEAR)
+# data = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+# [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+# [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
+
+ans = 0
+for line in data.splitlines():
+    i1 = line.index("]")
+    i2 = line.index("{")
+
+    target = [0 if c == "." else 1 for c in line[1:i1]]
+    buttons = [[int(n) for n in piece[1:-1].split(",")] for piece in line[i1+2:i2-1].split()]
+
+    target = sum(x * (2**i) for i, x in enumerate(target))
+    buttons = [sum(2**i for i in button) for button in buttons]
+
+    success = False
+    for n in range(1, len(buttons) + 1):
+        for bb in itertools.combinations(buttons, n):
+            result = 0
+            for b in bb:
+                result ^= b
+            if result == target:
+                ans += n
+                success = True
+                break
+        if success:
+            break
+
+print(ans)
+# aocd.submit(ans, part="a", day=DAY, year=YEAR)
